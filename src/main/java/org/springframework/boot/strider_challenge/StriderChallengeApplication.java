9a00eb5a02f609bd0a727c95ccbc76880c5d691e
vgvbvictor <vgvbvictor@gmail.com>
2018-08-03 13:29:18 -0300

    Initial commit

---
+++ src/main/java/org/springframework/boot/strider_challenge/StriderChallengeApplication.java
@@ -0,0 +1,12 @@
+package org.springframework.boot.strider_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StriderChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StriderChallengeApplication.class, args);
+	}
+}
